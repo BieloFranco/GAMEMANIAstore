529656fbada9333a6aa2abe4746eb94f5d340442
GabrieloFranco <g.franco@aluno.ifsp.edu.br>
2021-12-22 21:13:49 -0300

    22/12 21:13

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-//
-//document.querySelector("#slides")
-//.addEventListener("wheel", event=>{
-//    if(event.deltaY>0){
- //       event.target.scrollBy(300, 0)
- //   }else{
- //       event.target.scrollBy(-300, 0)
- //   }
-//})
-//
+
+function iniciaModal (modalID) {
+  const modal = document.getElementByID (modalID);
+  modal.classList.add('mostrar')
+  modal.addEventListener('click', () =>{
+    console.log('clicou modal')
+  })
+
+}
+const logo = document.querySelector('.logog')
+logo.addEventListener('click', function() {
+  iniciaModal('modal-promocao');
+
+})
 
 const scrollWheel = event => {
     if(event.deltaY < 0){
@@ -28,3 +32,4 @@ const scrollWheel = event => {
     document.querySelector("#slides").scrollTo(count, 0)
     console.log('moveu')
   }, 1500)
+
